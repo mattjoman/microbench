5c6c782b0f942c31df6a4d7783d36481f558e82e
Your Name <you@example.com>
2025-12-06 12:40:46 +0000

    Added metrics

--- src/bench.c
+++ src/bench.c
@@ -18,18 +18,44 @@ static uint64_t rdtscp() {
     return ((uint64_t)hi << 32) | lo;
 }
 
-static struct perf_event_attr create_perf_config() {
+#define METRIC_CPU_CYCLES 0
+#define METRIC_L1_CACHE_MISSES 1
+#define METRIC_BRANCH_MISPREDICTIONS 4
+#define METRIC_PAGE_FAULTS 16
+
+static struct perf_event_attr create_perf_config(int metric) {
     struct perf_event_attr pea;
+
     memset(&pea, 0, sizeof(struct perf_event_attr));
-    pea.type = PERF_TYPE_HW_CACHE;
-    pea.config = PERF_COUNT_HW_CACHE_L1D
-        | (PERF_COUNT_HW_CACHE_OP_READ << 8)
-        | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+
+    switch (metric) {
+        case METRIC_CPU_CYCLES:
+            pea.type = PERF_TYPE_HARDWARE;
+            pea.config = PERF_COUNT_HW_CPU_CYCLES;
+            break;
+        case METRIC_L1_CACHE_MISSES:
+            pea.type = PERF_TYPE_HW_CACHE;
+            pea.config = PERF_COUNT_HW_CACHE_L1D
+                | (PERF_COUNT_HW_CACHE_OP_READ << 8)
+                | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+            break;
+        case METRIC_BRANCH_MISPREDICTIONS:
+            pea.type = PERF_TYPE_HARDWARE;
+            pea.config = PERF_COUNT_HW_BRANCH_MISSES;
+            break;
+        case METRIC_PAGE_FAULTS:
+            pea.type = PERF_TYPE_SOFTWARE;
+            pea.config = PERF_COUNT_SW_PAGE_FAULTS;
+            break;
+        default:
+            break;
+    }
     pea.size = sizeof(struct perf_event_attr);
     pea.disabled = 0;
     pea.exclude_kernel = 1;
     pea.exclude_hv = 1;
     pea.exclude_idle = 1;
+
     return pea;
 }
 
@@ -51,10 +77,13 @@ uint64_t bench_cache_miss(void (*f)(void)) {
     int fd;
     uint64_t count;
 
-    pea = create_perf_config();
-    fd = syscall(SYS_perf_event_open, &pea, 0, -1, -1, 0);
+    pea = create_perf_config(METRIC_BRANCH_MISPREDICTIONS);
+
+    if ((fd = syscall(SYS_perf_event_open, &pea, 0, -1, -1, 0)) == -1)
+        exit(1);
     f();
     read(fd, &count, sizeof(count));
+
     close(fd);
 
     return count;
