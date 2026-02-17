abcf7ccb744944c1a0149cd89c6bbba9028701f3
Your Name <you@example.com>
2026-02-17 19:34:30 +0000

    User passes string args rather than numbers for clarity

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <getopt.h>
 
 #include "../include/batch.h"
@@ -8,38 +9,46 @@
 
 int main(int argc, char *argv[]) {
 
-    int workload_id = -1;
-    int ctr_grp_id = -1;
+    char *workload_str = NULL;
+    char *ctr_grp_str  = NULL;
 
     static struct option long_opts[] = {
         {"workload",    required_argument, 0, 'w'},
-        {"ctr-grp", required_argument, 0, 'e'},
+        {"ctr-grp", required_argument, 0, 'c'},
         {0, 0, 0, 0}
     };
 
     int opt;
 
-    while ((opt = getopt_long(argc, argv, "w:e:", long_opts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "w:c:", long_opts, NULL)) != -1) {
         switch (opt) {
             case 'w':
-                workload_id = atoi(optarg);
+                //workload_id = atoi(optarg);
+                workload_str = optarg;
                 break;
-            case 'e':
-                ctr_grp_id = atoi(optarg);
+            case 'c':
+                ctr_grp_str = optarg;
+                //ctr_grp_id = atoi(optarg);
                 break;
             default:
-                fprintf(stderr, "Usage\n");
+                fprintf(stderr, "Usage 1\n");
                 return 1;
         }
     }
 
-    if (workload_id < 0 || workload_id >= NUMBER_OF_WORKLOADS) {
-        fprintf(stderr, "Invalid Workload: %d\n", workload_id);
-        return 1;
+    int workload_id = -1;
+    int ctr_grp_id = -1;
+
+    if (strcmp(workload_str, "contiguous-array") == 0) {
+        workload_id = WL_CONTIGUOUS_ARRAY;
     }
 
-    if (ctr_grp_id < 0 || ctr_grp_id >= NUMBER_OF_COUNTER_GRPS) {
-        fprintf(stderr, "Invalid Counter Group: %d\n", ctr_grp_id);
+    if (strcmp(ctr_grp_str, "ipc") == 0) {
+        ctr_grp_id = COUNTER_GRP_IPC;
+    }
+
+    if (workload_id < 0 || ctr_grp_id < 0) {
+        fprintf(stderr, "Usage 2\n");
         return 1;
     }
 
