e01e631d5afc54b82d42f96e42ca7b2a0b37a8c3
Your Name <you@example.com>
2026-02-20 20:16:04 +0000

    Counter -> metric

--- src/main.c
+++ src/main.c
@@ -13,31 +13,31 @@ static const char help_text[] =
 "Options:\n"
 "  -h, --help                       Display this message\n"
 "  -w, --workload WORKLOAD          Select workload to benchmark\n"
-"  -c, --counter-group GROUP        Select a group of metrics to record\n"
+"  -m, --metric-group GROUP         Select a group of metrics to record\n"
 "\n"
 "Workloads:\n"
 "  contiguous-array                 Process a contiguous in array\n"
 "  scattered-array                  Process an array of heap in pointers\n"
 "\n"
-"Counter groups:\n"
+"Metric groups:\n"
 "  ipc                              Record cycles & instructions to get ipc\n"
 "";
 
 int main(int argc, char *argv[]) {
 
     char *workload_str = NULL;
-    char *ctr_grp_str  = NULL;
+    char *metric_grp_str  = NULL;
 
     static struct option long_opts[] = {
         {"help",    required_argument, 0, 'h'},
         {"workload",    required_argument, 0, 'w'},
-        {"ctr-grp", required_argument, 0, 'c'},
+        {"metric-grp", required_argument, 0, 'm'},
         {0, 0, 0, 0}
     };
 
     int opt;
 
-    while ((opt = getopt_long(argc, argv, "w:c:h", long_opts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "w:m:h", long_opts, NULL)) != -1) {
         switch (opt) {
             case 'h':
                 fputs(help_text, stdout);
@@ -45,8 +45,8 @@ int main(int argc, char *argv[]) {
             case 'w':
                 workload_str = optarg;
                 break;
-            case 'c':
-                ctr_grp_str = optarg;
+            case 'm':
+                metric_grp_str = optarg;
                 break;
             default:
                 fprintf(stderr, "Usage 1\n");
@@ -55,17 +55,19 @@ int main(int argc, char *argv[]) {
     }
 
     int workload_id = -1;
-    int ctr_grp_id = -1;
+    int metric_grp_id = -1;
 
     if (strcmp(workload_str, "contiguous-array") == 0) {
         workload_id = WL_CONTIGUOUS_ARRAY;
+    } else if (strcmp(workload_str, "scattered-array") == 0) {
+        workload_id = WL_SCATTERED_ARRAY;
     }
 
-    if (strcmp(ctr_grp_str, "ipc") == 0) {
-        ctr_grp_id = METRIC_GRP_IPC;
+    if (strcmp(metric_grp_str, "ipc") == 0) {
+        metric_grp_id = METRIC_GRP_IPC;
     }
 
-    if (workload_id < 0 || ctr_grp_id < 0) {
+    if (workload_id < 0 || metric_grp_id < 0) {
         fprintf(stderr, "Usage 2\n");
         return 1;
     }
@@ -74,7 +76,7 @@ int main(int argc, char *argv[]) {
         .warmup_runs = 5,
         .batch_runs = MAX_BATCH_SIZE,
         .workload_id = workload_id,
-        .metric_grp_id = ctr_grp_id,
+        .metric_grp_id = metric_grp_id,
     };
     run_batch(batch_conf);
 
