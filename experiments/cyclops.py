86b83752b7e88b9bfb8ac80254f6435b27c3a3d2
Matthew Penney <matt@matthewpenney.net>
2026-04-28 01:48:52 +0100

    Updated experiment scripts

--- experiments/cyclops.py
+++ experiments/cyclops.py
@@ -2,22 +2,21 @@ import subprocess
 from dataclasses import dataclass, field
 
 @dataclass
-class BatchRunner:
+class ParamSweep:
+    key: int
+    low: int
+    high: int
+    step: int
+
+@dataclass
+class Cyclops:
     workload: str
     metric_grp: str
     warmup_runs: int = 5
     batch_runs: int = 5
     params: dict = field(default_factory=dict)
-
-    def build_output_file_name(self) -> str:
-        file_name = f"{self.workload}"
-        file_name += f"__{self.metric_grp}"
-
-        for key, val in self.params.items():
-            file_name += f"__{key}_{val}"
-
-        file_name += ".csv"
-        return file_name
+    param_sweep: ParamSweep | None = None
+    write_per_batch_csv_files: bool = False
 
     def exec(self):
         arg_list = [
@@ -26,21 +25,32 @@ class BatchRunner:
             "-m", self.metric_grp,
             "-u", str(self.warmup_runs),
             "-r", str(self.batch_runs),
-            "-o", self.build_output_file_name(),
         ]
+
+        if self.write_per_batch_csv_files:
+            arg_list.append("-o")
 
         for key, val in self.params.items():
             arg_list.append("-p")
             arg_list.append(f"{key}={val}")
+
+        if self.param_sweep:
+            arg_list.append("-s")
+            arg_list.append(
+                f"{self.param_sweep.key}="
+                f"{self.param_sweep.low}:"
+                f"{self.param_sweep.high}:"
+                f"{self.param_sweep.step}:"
+            )
 
         result = subprocess.run(arg_list)
         print(result.stdout)
 
 if __name__ == "__main__":
     workload_params = { "array-elements": 100 }
-    batch = BatchRunner(
+    cyclops = Cyclops(
         workload="STRIDED_ARRAY",
         metric_grp="IPC",
         params=workload_params,
     )
-    batch.exec()
+    cyclops.exec()
--- experiments/estimate_cache_size.py
+++ experiments/estimate_cache_size.py
@@ -1,130 +1,75 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from cyclops import BatchRunner
+from cyclops import Cyclops, ParamSweep
 
 BATCH_RUNS = 50
 WARMUP_RUNS = 5
 
-def run_L1D_batches(array_sizes: list):
+#AGGREGATE = "MIN"
+#AGGREGATE = "MAX"
+AGGREGATE = "MEDIAN"
 
-    x_L1D = []
-    y_L1D = []
+def run_L1D_experiment():
 
-    for size in array_sizes:
+    param_sweep = ParamSweep(
+        key="array-elements",
+        low=100,
+        high=10000,
+        step=10,
+    )
 
-        params = { "array-elements": size }
-        batch = BatchRunner(
-            workload="STRIDED_ARRAY",
-            metric_grp="L1D_READS",
-            warmup_runs=WARMUP_RUNS,
-            batch_runs=BATCH_RUNS,
-            params=params,
-        )
-        batch.exec()
+    cyclops = Cyclops(
+        workload="STRIDED_ARRAY",
+        metric_grp="L1D_READS",
+        warmup_runs=WARMUP_RUNS,
+        batch_runs=BATCH_RUNS,
+        param_sweep=param_sweep,
+    )
+    cyclops.exec()
 
-        df = pd.read_csv(
-            batch.build_output_file_name(),
-            comment="#",
-            index_col="METRIC"
-        )
-        x_L1D.append(batch.params.get("array-elements", 0) * 64)
-        y_L1D.append(df.loc["L1D_READ_MISS_RATE", "MEDIAN"])
+    df = pd.read_csv(
+        "L1D_READ_MISS_RATE.csv",
+        comment="#",
+        index_col=param_sweep.key
+    )
 
-    return x_L1D, y_L1D
+    return 64 * df.index.values, df[AGGREGATE].values
 
-def run_LLC_batches(array_sizes: list):
+def run_LLC_experiment():
 
-    x_LLC = []
-    y_LLC = []
+    param_sweep = ParamSweep(
+        key="array-elements",
+        low=1000,
+        high=200000,
+        step=1000,
+    )
 
-    for size in array_sizes:
+    cyclops = Cyclops(
+        workload="STRIDED_ARRAY",
+        metric_grp="LLC_READS",
+        warmup_runs=WARMUP_RUNS,
+        batch_runs=BATCH_RUNS,
+        param_sweep=param_sweep,
+    )
+    cyclops.exec()
 
-        params = { "array-elements": size }
-        batch = BatchRunner(
-            workload="STRIDED_ARRAY",
-            metric_grp="LLC_READS",
-            warmup_runs=WARMUP_RUNS,
-            batch_runs=BATCH_RUNS,
-            params=params,
-        )
-        batch.exec()
+    df = pd.read_csv(
+        "LLC_READ_MISS_RATE.csv",
+        comment="#",
+        index_col=param_sweep.key
+    )
 
-        df = pd.read_csv(
-            batch.build_output_file_name(),
-            comment="#",
-            index_col="METRIC"
-        )
-
-        x_LLC.append(batch.params.get("array-elements", 0) * 64)
-        y_LLC.append(df.loc["LLC_READ_MISS_RATE", "MEDIAN"])
-
-    return x_LLC, y_LLC
+    return 64 * df.index.values, df[AGGREGATE].values
 
 if __name__ == "__main__":
-    array_elements = [
-        10,
-        30,
-        50,
-        80,
-        90,
 
-        100,
-        150,
-        175,
-        200,
-        225,
-        250,
-        275,
-        300,
-        325,
-        350,
-        375,
-        400,
-        500,
-        800,
-
-        1000,
-        3000,
-        3500,
-        4000,
-        5000,
-        5500,
-        6000,
-        8000,
-
-        10000,
-        15000,
-        20000,
-        25000,
-        30000,
-        32500,
-        35000,
-        37500,
-        40000,
-        42500,
-        45000,
-        47500,
-        50000,
-        80000,
-
-        100000,
-        300000,
-        500000,
-        800000,
-
-        #1000000,
-        #3000000,
-        #5000000,
-        #8000000,
-    ]
-
-    x_LLC, y_LLC = run_LLC_batches(array_elements)
-    x_L1D, y_L1D = run_L1D_batches(array_elements)
+    x_L1D, y_L1D = run_L1D_experiment()
+    x_LLC, y_LLC = run_LLC_experiment()
 
     plt.figure()
-    plt.plot(x_LLC, y_LLC, marker="", label="LLC")
     plt.plot(x_L1D, y_L1D, marker="", label="L1D")
+    plt.plot(x_LLC, y_LLC, marker="", label="LLC")
     plt.xscale("log")
     plt.yscale("log")
     plt.xlabel("Array size (Bytes)")
